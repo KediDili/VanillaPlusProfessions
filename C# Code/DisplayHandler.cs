731e60c3c0c3b3d2086c648a1593417f3315de8d
AlanBF <alanbf1992@gmail.com>
2025-08-23 23:57:54 -0400

    Fix LittlePlus error when getting a level up to lvl 11 in the menu

--- C# Code/DisplayHandler.cs
+++ C# Code/DisplayHandler.cs
@@ -385,6 +385,10 @@ namespace VanillaPlusProfessions
                     }
                     if (CoreUtility.IsOverlayValid())
                     {
+                        if (LittlePlus is null)
+                        {
+                            HandleSkillPage(page, Game1.activeClickableMenu);
+                        }
                         LittlePlus.draw(e.SpriteBatch);
                         page.drawMouse(e.SpriteBatch);
                     }
@@ -499,6 +503,10 @@ namespace VanillaPlusProfessions
 
                     if (CoreUtility.IsOverlayValid())
                     {
+                        if (LittlePlus is null)
+                        {
+                            HandleSkillPage(page2, Game1.activeClickableMenu);
+                        }
                         LittlePlus.draw(e.SpriteBatch);
                         page2.drawMouse(e.SpriteBatch);
                     }
